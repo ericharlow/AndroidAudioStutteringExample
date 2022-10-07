fb0702531d3622bf89d0623ef83fe14ddeaf166c
Eric Harlow <eharlow@midwesttapes.com>
2022-10-07 16:11:33 -0600

    Changelog: - downloading dash content

--- app/src/main/java/com/example/androidaudiostutteringexample/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/androidaudiostutteringexample/AudioPlayerActivity.kt
@@ -4,11 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import com.google.android.exoplayer2.demo.DemoDownloadService
+import com.google.android.exoplayer2.offline.DownloadService
 
 class AudioPlayerActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_audio_player)
+    startDownloadService()
     ContextCompat.startForegroundService(applicationContext, Intent(applicationContext, AudioService::class.java))
+  }
+
+  /** Start the download service if it should be running but it's not currently.  */
+  private fun startDownloadService() {
+    // Starting the service in the foreground causes notification flicker if there is no scheduled
+    // action. Starting it in the background throws an exception if the app is in the background too
+    // (e.g. if device screen is locked).
+    try {
+      DownloadService.start(this, DemoDownloadService::class.java)
+    } catch (e: IllegalStateException) {
+      DownloadService.startForeground(this, DemoDownloadService::class.java)
+    }
   }
 }
--- app/src/main/java/com/example/androidaudiostutteringexample/AudioService.kt
+++ app/src/main/java/com/example/androidaudiostutteringexample/AudioService.kt
@@ -5,41 +5,20 @@ import android.app.Notification
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.os.Debug
 import android.support.v4.media.MediaBrowserCompat
-import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.drm.DrmSession.DrmSessionException
-import com.google.android.exoplayer2.drm.DrmSessionEventListener
-import com.google.android.exoplayer2.drm.OfflineLicenseHelper
-import com.google.android.exoplayer2.offline.*
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.source.dash.DashMediaSource
-import com.google.android.exoplayer2.source.dash.offline.DashDownloader
-import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
-import com.google.android.exoplayer2.upstream.cache.CacheDataSource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.io.IOException
-import java.util.*
-
+import com.google.android.exoplayer2.demo.DemoUtil
+import com.google.android.exoplayer2.demo.IntentUtil
 
 class AudioService : MediaBrowserServiceCompat() {
 
   private lateinit var notificationManager: NotificationManager
 
   private lateinit var player: ExoPlayer
-
-  private lateinit var cacheDataSourceFactory: DataSource.Factory
-
-  private lateinit var renderersFactory: RenderersFactory
 
   private val analyticsListener = AudioAnalyticsListener()
 
@@ -49,27 +28,12 @@ class AudioService : MediaBrowserServiceCompat() {
     super.onCreate()
     notificationManager =
       applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-    renderersFactory = DefaultRenderersFactory(this).forceEnableMediaCodecAsynchronousQueueing()
-
-    cacheDataSourceFactory = CacheDataSource.Factory()
-      .setCache(application.asApp().downloadCache)
-      .setUpstreamDataSourceFactory(DefaultHttpDataSource.Factory()) // DefaultDataSource.Factory(this)
-      .setCacheWriteDataSinkFactory(null) // Disable writing.
-
-    player = ExoPlayer.Builder(this, renderersFactory)
-      .setWakeMode(C.WAKE_MODE_LOCAL)
-      .setMediaSourceFactory(
-        DefaultMediaSourceFactory(this).setDataSourceFactory(cacheDataSourceFactory))
-      .setRenderersFactory(renderersFactory)
-      .build()
   }
 
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
     if (!serviceForegrounded) internalStartForeground(createPlaceholderNotification())
 
-//    downloadAndPlayContentSimpleApproach()
-    downloadAndPlayContentMoreComplexApproach()
+    downloadAndPlayContent()
     return super.onStartCommand(intent, flags, startId)
   }
 
@@ -94,173 +58,82 @@ class AudioService : MediaBrowserServiceCompat() {
 
   // Private helper functions
 
-  private fun downloadAndPlayContentSimpleApproach() {
-    CoroutineScope(Dispatchers.IO).launch {
-      try {
-        val app = application.asApp()
-        val cache = app.downloadCache
-        val cacheDataSourceFactory = CacheDataSource.Factory()
-          .setCache(cache)
-          .setUpstreamDataSourceFactory(DefaultHttpDataSource.Factory())
-        // Create a downloader for the first representation of the first adaptation set of the first
-        // period.
-        val initialMediaItem = MediaItem.Builder()
-          .setUri("https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd")
-          .setStreamKeys(Collections.singletonList(StreamKey(0, 0, 0)))
-          .build()
-        val dashDownloader = DashDownloader(
-          initialMediaItem,
-          cacheDataSourceFactory)
-        // Perform the download.
-        dashDownloader.download { contentLength, bytesDownloaded, percentDownloaded ->
-          if (percentDownloaded == 100.0F)
-          Log.i("SBSS", "onStartCommand: download percent: $percentDownloaded")
-        }
-
-        val mediaSource: DashMediaSource =
-          DashMediaSource.Factory(cacheDataSourceFactory).createMediaSource(initialMediaItem)
-
-        CoroutineScope(Dispatchers.Main).launch {
-          try {
-            player.setMediaSource(mediaSource)
-
-            player.playWhenReady = true
-            player.playbackParameters = PlaybackParameters(1.75F, 1.0F)
-            player.addAnalyticsListener(analyticsListener)
-            player.prepare()
-          } catch(t: Throwable) {
-            Log.e("SBSS", "downloadAndPlayContent: ", t)
-          }
-        }
-      } catch(t: Throwable) {
-        Log.e("SBSS", "downloadAndPlayContent: ", t)
-      }
-    }
+  private fun downloadAndPlayContent() {
+    Debug.waitForDebugger()
+    // download content
+    val renderersFactory = DemoUtil.buildRenderersFactory(this, false)
+    val downloadTracker = DemoUtil.getDownloadTracker(this)
+    downloadTracker.toggleDownload(null, getInitialMediaItem(), renderersFactory)
+    // TODO: wait for download to complete
+    // prepare playback
+//    val intent = getIntent()
+//    val mediaItems = createMediaItems(intent)
   }
 
-  private fun downloadAndPlayContentMoreComplexApproach() {
-//        Debug.waitForDebugger()
-    val app = application.asApp()
-
-    val downloadManager = DownloadManager(
-      this,
-      app.databaseProvider,
-      app.downloadCache,
-      cacheDataSourceFactory,
-      Runnable::run)
-
-    val initialMediaItem = buildInitialMediaItem()
-    val dataSourceFactory = DefaultHttpDataSource.Factory()
-    val offlineLicenseHelper = OfflineLicenseHelper.newWidevineInstance(
-      initialMediaItem.localConfiguration?.drmConfiguration?.licenseUri.toString(),
-      dataSourceFactory,
-      DrmSessionEventListener.EventDispatcher())
-    val downloadHelper: DownloadHelper = DownloadHelper.forMediaItem(
-      this,
-      initialMediaItem,
-      DefaultRenderersFactory(this),
-      dataSourceFactory)
-    downloadHelper.prepare(object : DownloadHelper.Callback {
-      override fun onPrepared(helper: DownloadHelper) {
-        val format = getFirstFormatWithDrmInitData(helper)
-        val keySetId = try {
-          offlineLicenseHelper.downloadLicense(format!!)
-        } catch (e: DrmSessionException) {
-          Log.e("SBSS", "DownloadHelper.onPrepared: ", e)
-          ByteArray(0)
-        } finally {
-          offlineLicenseHelper.release()
-        }
-        val data = "HD (cenc)".toByteArray(Charsets.UTF_8)
-        val downloadRequest = helper.getDownloadRequest(data).copyWithKeySetId(keySetId)
-        downloadManager.addDownload(downloadRequest)
-        downloadManager.resumeDownloads()
-        downloadManager.addListener(object : DownloadManager.Listener {
-          override fun onDownloadChanged(
-            downloadManager: DownloadManager, download: Download, finalException: Exception?) {
-            Log.i("SBSS", "onDownloadChanged: ")
-            if (download.request.id == downloadRequest.id && download.state == Download.STATE_COMPLETED) {
-              val mediaItem = buildMediaItem(download.request)
-              val progressiveMediaSource = ProgressiveMediaSource.Factory(cacheDataSourceFactory)
-                .createMediaSource(mediaItem)
-              player.setMediaSource(progressiveMediaSource)
-
-              player.playWhenReady = true
-              player.playbackParameters = PlaybackParameters(1.75F, 1.0F)
-              player.addAnalyticsListener(analyticsListener)
-              player.prepare()
-
-//              try { downloadHelper.release() } catch(t: Throwable) {}
-            }
-          }
-        })
-      }
-
-      override fun onPrepareError(helper: DownloadHelper, e: IOException) {
-        Log.e("SBSS", "onPrepareError: ", e)
-        helper.release()
-      }
-    })
-  }
-
-  private fun buildMediaItem(downloadRequest: DownloadRequest): MediaItem {
-    val mediaItemBuilder = MediaItem.Builder()
-      .setMediaId(downloadRequest.id)
-      .setUri(downloadRequest.uri)
-      .setMimeType(downloadRequest.mimeType)
-      .setMediaMetadata(MediaMetadata.Builder().setTitle("HD (cenc)").build())
-      .setDrmConfiguration(
-        MediaItem.DrmConfiguration.Builder(C.WIDEVINE_UUID)
-          .setLicenseUri("https://proxy.uat.widevine.com/proxy?video_id=2015_tears&provider=widevine_test")
-          .setLicenseRequestHeaders(emptyMap())
-          .setKeySetId(downloadRequest.keySetId)
-          .build())
-      .setStreamKeys(downloadRequest.streamKeys)
-    return mediaItemBuilder.build()
-  }
-
-  private fun buildInitialMediaItem(): MediaItem {
-    val contentUri = Uri.parse("https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd")
-    val licenseUri = Uri.parse("https://proxy.uat.widevine.com/proxy?video_id=2015_tears&provider=widevine_test")
-    val mediaItemBuilder = MediaItem.Builder()
-      .setUri(contentUri)
+  private fun getInitialMediaItem(): MediaItem {
+    return MediaItem.Builder()
+      .setUri("https://storage.googleapis.com/wvmedia/cenc/h264/tears/tears.mpd")
       .setMimeType("application/dash+xml")
       .setMediaMetadata(MediaMetadata.Builder().setTitle("HD (cenc)").build())
       .setDrmConfiguration(
         MediaItem.DrmConfiguration.Builder(C.WIDEVINE_UUID)
-          .setLicenseUri(licenseUri)
-          .setLicenseRequestHeaders(emptyMap())
-//          .setKeySetId(keySetId)
+          .setLicenseUri("https://proxy.uat.widevine.com/proxy?video_id=2015_tears&provider=widevine_test")
+          .setForceDefaultLicenseUri(false)
+          .setMultiSession(false)
+          .setPlayClearContentWithoutKey(false)
           .build()
-      )
-//      .setStreamKeys(streamKeys)
-    return mediaItemBuilder.build()
+      ).build()
   }
 
-  // Internal methods.
-  private fun getFirstFormatWithDrmInitData(helper: DownloadHelper): Format? {
-    for (periodIndex in 0 until helper.periodCount) {
-      val mappedTrackInfo = helper.getMappedTrackInfo(periodIndex)
-      for (rendererIndex in 0 until mappedTrackInfo.rendererCount) {
-        val trackGroups = mappedTrackInfo.getTrackGroups(rendererIndex)
-        for (trackGroupIndex in 0 until trackGroups.length) {
-          val trackGroup = trackGroups[trackGroupIndex]
-          for (formatIndex in 0 until trackGroup.length) {
-            val format = trackGroup.getFormat(formatIndex)
-            if (format.drmInitData != null) {
-              return format
-            }
-          }
-        }
-      }
-    }
-    return null
+  private fun getIntent(): Intent {
+    val intent = Intent()
+    val mediaItem = getInitialMediaItem()
+    IntentUtil.addToIntent(listOf(mediaItem), intent)
+    intent.putExtra(IntentUtil.PREFER_EXTENSION_DECODERS_EXTRA, false)
+    return intent
   }
+
+//  private fun createMediaItems(intent: Intent): List<MediaItem> {
+//    val action = intent.action
+//    val actionIsListView = IntentUtil.ACTION_VIEW_LIST == action
+//    if (!actionIsListView && IntentUtil.ACTION_VIEW != action) {
+//      Log.i("SBSS", "createMediaItems: unexpected intent action")
+//      return emptyList()
+//    }
+//    // Note: important to have download before here
+//    val mediaItems: List<MediaItem> =
+//      com.google.android.exoplayer2.demo.PlayerActivity.createMediaItems(
+//        intent,
+//        DemoUtil.getDownloadTracker( /* context= */this)
+//      )
+//    for (i in mediaItems.indices) {
+//      val mediaItem = mediaItems[i]
+//      if (!Util.checkCleartextTrafficPermitted(mediaItem)) {
+//        Log.i("SBSS", "createMediaItems: clear text network traffic not permitted")
+//        return emptyList()
+//      }
+//      if (Util.maybeRequestReadExternalStoragePermission( /* activity= */this, mediaItem)) {
+//        // The player will be reinitialized if the permission is granted.
+//        return emptyList()
+//      }
+//      val drmConfiguration = mediaItem.localConfiguration!!.drmConfiguration
+//      if (drmConfiguration != null) {
+//        if (Build.VERSION.SDK_INT < 18) {
+//          Log.i("SBSS", "createMediaItems: drm unsupported")
+//          return emptyList()
+//        } else if (!FrameworkMediaDrm.isCryptoSchemeSupported(drmConfiguration.scheme)) {
+//          Log.i("SBSS", "createMediaItems: drm scheme unsupported")
+//          return emptyList()
+//        }
+//      }
+//    }
+//    return mediaItems
+//  }
 
   private fun createPlaceholderNotification(): Notification =
     NotificationCompat.Builder(this, "playback_controls_notification_channel")
       .setSmallIcon(R.drawable.ic_baseline_library_music_24)
-      .setContentText(getString(R.string.starting_playback_message))
+      .setContentText(getString(R.string.preparing_playback_message))
       .build()
 
   private fun internalStartForeground(notification: Notification) {
